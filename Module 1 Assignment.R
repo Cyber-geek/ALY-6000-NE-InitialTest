bd1389df94f38ed624e6e76a37c1d3fa1e10d400
S Jeevan Rishi Kumar <43720824+Cyber-geek@users.noreply.github.com>
2022-01-16 14:55:24 +0530

    Update Module 1 Assignment.R

--- Module 1 Assignment.R
+++ Module 1 Assignment.R
@@ -1,1 +1,24 @@
+#1.Print your name at the top of the script
 print("Jeevan Rishi Kumar Sunkara")
+ 
+#2.Install the vcd package
+install.packages("vcd")
+
+#3.Import the vcd library (pg 19) 
+library(vcd)
+
+#4.Plot a sales ~ temp scatter plot using the data below:
+#Sales data: (7,11,15,20,19,11,18,10,6,22) 
+#Temperature data: (69,81,77,84,80,97,87,70,65,90) 
+Sales = c(7,11,15,20,19,11,18,10,6,22)
+Temperature = c(69,81,77,84,80,97,87,70,65,90)
+plot(Sales,Temperature)
+
+#5.Find the mean temperature
+mean(Temperature)
+
+#6.Delete the 3rd element from the sales vector
+Sales[-3]
+
+
+
