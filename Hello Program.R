977b88fda3066e1169d1e89d5dcc3288b6fa380b
S Jeevan Rishi Kumar <43720824+Cyber-geek@users.noreply.github.com>
2022-01-16 14:23:51 +0530

    Create Hello Program.R

---
+++ Hello Program.R
@@ -0,0 +1,20 @@
+#Initial exercise
+age = c(1,3,5,2,11,9,3,9,12,3)
+weight = c(4.4,5.3,7.2,5.2,8.5,7.3,6,10.4,10.2,6.1)
+mean(weight)
+sd(weight)
+cor(age,weight)
+plot(age,weight)
+
+#Working with Packages
+help.start()
+install.packages("vcd")
+help(package = "vcd")
+library(vcd)
+help(Arthritis)
+Arthritis
+example(Arthritis)
+
+#ggplot2
+library(ggplot2)
+
